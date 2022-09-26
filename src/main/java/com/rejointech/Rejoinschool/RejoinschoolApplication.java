a89be705b0a4c9b9e0abe049db8b29910063d9fa
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-09-26 05:39:20 +0530

    first commit

---
+++ src/main/java/com/rejointech/Rejoinschool/RejoinschoolApplication.java
@@ -0,0 +1,13 @@
+package com.rejointech.Rejoinschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RejoinschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RejoinschoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rejointech/Rejoinschool/RejoinschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rejointech.Rejoinschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RejoinschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
