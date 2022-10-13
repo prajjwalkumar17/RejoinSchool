4167e8410aa178b0af4c032fd97c3c2d70ee9270
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-10-13 22:41:05 +0530

    done with the holiday getparam and path variable going on for validations

--- src/main/java/com/rejointech/Rejoinschool/controller/HolidaysController.java
+++ src/main/java/com/rejointech/Rejoinschool/controller/HolidaysController.java
@@ -4,6 +4,8 @@ import com.rejointech.Rejoinschool.model.Holidays;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,9 +13,37 @@ import java.util.stream.Collectors;
 
 @Controller
 public class HolidaysController {
-    @GetMapping("")
-    public String displayHolidays(Model model){
-        List<Holidays> holiday= Arrays.asList(
+//    @GetMapping("/holidays")
+//    public String displayHolidays(@RequestParam(required = false)boolean festival,
+//                                  @RequestParam(required = false)boolean federal,Model model){
+//        model.addAttribute("festival",festival);
+//        model.addAttribute("federal",federal);
+//        List<Holidays> holidays= Arrays.asList(
+//                new Holidays("17 feb 2020","diwali" , Holidays.Type.FESTIVAL),
+//                new Holidays("18 oct 2020","holi" , Holidays.Type.FESTIVAL),
+//                new Holidays("19 oct 2021","labor day" , Holidays.Type.FEDERAL),
+//                new Holidays("27 jan 2022","chatth" , Holidays.Type.FESTIVAL),
+//                new Holidays("21 nov 2023","bank" , Holidays.Type.FEDERAL),
+//                new Holidays("12 oct 2024","baishakhi" , Holidays.Type.FESTIVAL),
+//                new Holidays("13 dec 2020","mayday" , Holidays.Type.FEDERAL));
+//
+//                for(Holidays.Type type:Holidays.Type.values()){
+//                    model.addAttribute(type.toString(),
+//                            (holidays.stream().filter(holiday -> holiday.getType().equals(type))).collect(Collectors.toList()));
+//                }
+//                return "holidays.html";
+//    }
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display, Model model){
+        if(display!=null && display.equals("all")) {
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        }
+        else if(display!=null && display.equals("federal"))
+            model.addAttribute("federal",true);
+        else if(display!=null && display.equals("festival"))
+            model.addAttribute("festival",true);
+        List<Holidays> holidays= Arrays.asList(
                 new Holidays("17 feb 2020","diwali" , Holidays.Type.FESTIVAL),
                 new Holidays("18 oct 2020","holi" , Holidays.Type.FESTIVAL),
                 new Holidays("19 oct 2021","labor day" , Holidays.Type.FEDERAL),
@@ -22,10 +52,10 @@ public class HolidaysController {
                 new Holidays("12 oct 2024","baishakhi" , Holidays.Type.FESTIVAL),
                 new Holidays("13 dec 2020","mayday" , Holidays.Type.FEDERAL));
 
-//                for(Holidays.Type type:Holidays.Type.values()){
-//                    model.addAttribute(type.toString(),
-//                            (holiday.stream().filter(holiday -> holiday.getType().equals(type))).collect(Collectors.toCollection((collec))))
-//                }
+                for(Holidays.Type type:Holidays.Type.values()){
+                    model.addAttribute(type.toString(),
+                            (holidays.stream().filter(holiday -> holiday.getType().equals(type))).collect(Collectors.toList()));
+                }
                 return "holidays.html";
     }
 }
--- src/main/java/com/rejointech/Rejoinschool/model/Holidays.java
+++ src/main/java/com/rejointech/Rejoinschool/model/Holidays.java
@@ -1,30 +1,14 @@
 package com.rejointech.Rejoinschool.model;
 
+import lombok.Data;
+
+@Data
 public class Holidays {
     private final String day;
     private final String reason;
     private final Type type;
 
-
     public enum Type{
         FESTIVAL,FEDERAL
-    }
-
-    public Holidays(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
     }
 }
