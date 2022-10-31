1aff6cdd74630e3635349336e28a5169158807f6
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-10-31 21:54:25 +0530

    made the session scope

--- src/main/java/com/rejointech/Rejoinschool/service/ContactService.java
+++ src/main/java/com/rejointech/Rejoinschool/service/ContactService.java
@@ -2,22 +2,39 @@ package com.rejointech.Rejoinschool.service;
 
 import com.rejointech.Rejoinschool.controller.contactController;
 import com.rejointech.Rejoinschool.model.Contact;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
+@Slf4j
 @Service
+@SessionScope
 public class ContactService {
     /*
-    * Save Contact details into DB
-    * @Param Contact
-    * @return boolean
-    * */
-    private static Logger log= LoggerFactory.getLogger(contactController.class);
+     * Save Contact details into DB
+     * @Param Contact
+     * @return boolean
+     * */
+    private int counter=0;
+    public ContactService(){
+        System.out.println("Contact Service class initialized");
+    }
     public boolean saveMessageDetails(Contact contact){
         boolean isSaved=true;
-        System.out.println("hello");
         log.info(contact.toString());
         return isSaved;
     }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 }
+
