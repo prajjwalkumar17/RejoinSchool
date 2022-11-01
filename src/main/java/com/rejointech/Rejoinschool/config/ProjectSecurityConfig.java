123d500e1334bd071232435e086a8dbe0cd52ebb
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-11-01 16:14:02 +0530

    going on with login

---
+++ src/main/java/com/rejointech/Rejoinschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,40 @@
+package com.rejointech.Rejoinschool.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class ProjectSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().denyAll()
+//                .and().formLogin()
+//                .and().httpBasic();
+//    }
+        @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .mvcMatchers("/home").authenticated()
+                .mvcMatchers("/holidays/**").permitAll()
+                .mvcMatchers("/contact").permitAll()
+                .mvcMatchers("/saveMsg").permitAll()
+//                .mvcMatchers("/courses").authenticated()
+                .mvcMatchers("/courses").permitAll()
+                .mvcMatchers("/about").permitAll()
+                .and().formLogin().and().httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("1234").roles("user")
+                .and().withUser("admin").password("4321").roles("user","admin")
+                .and().passwordEncoder(NoOpPasswordEncoder.getInstance());
+
+        ;
+    }
+}
