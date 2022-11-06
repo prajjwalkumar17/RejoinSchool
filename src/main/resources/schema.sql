776429d8a7d4c317698da7f1b2207770c460f23e
prajjwalkumar17 <talk2prajjwal@gmail.com>
2022-11-06 13:13:04 +0530

    going on with database

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE IF NOT EXISTS 'contact_msg'(
 'status' varchar(10) NOT NULL,
 'created_at' TIMESTAMP NOT NULL,
 'updated_at' TIMESTAMP DEFAULT NULL,
+
 'updated_by' varchar(50) DEFAULT NULL
 );
